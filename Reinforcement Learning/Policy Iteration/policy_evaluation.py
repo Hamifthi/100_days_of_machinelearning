ff2a19d4cf7f2dc8b25358f3939ae97ae0e7e251
Hamifthi <hamedonil@yahoo.com>
2018-10-02 12:44:29 +0330

    improve solution for policy evaluation problem

--- Reinforcement Learning/Policy Iteration/policy_evaluation.py
+++ Reinforcement Learning/Policy Iteration/policy_evaluation.py
@@ -1,28 +1,25 @@
 import numpy as np
+import time
 from gridworld import GridworldEnv
 
 env = GridworldEnv()
 
 def policy_evaluation(policy, env, gamma = 1.0, theta = 0.00001):
     V = np.zeros(env.nS)
-    delta = 0
+    v_old = np.zeros(env.nS)
     while True:
         for state in range(env.nS):
             v = 0
             for action, action_probability in enumerate(policy[state]):
                 for probability, next_state, reward, done in env.P[state][action]:
                     v += action_probability * probability * (reward + gamma * V[next_state])
-            if v != 0:
-                if delta != 0:
-                    delta = min(delta, np.abs(v - V[state]))
-                else:
-                    delta = np.abs(v - V[state])
+            v_old[state] = V[state]
             V[state] = v
-        if theta > delta:
+        if theta > np.amax(np.abs(V - v)):
             break
     return np.array(V)
 
 if __name__ == '__main__':
     random_policy = np.ones([env.nS, env.nA]) / env.nA
-    v = policy_evaluation(random_policy, env)
-    print(v)
+    v_values = policy_evaluation(random_policy, env)
+    print(v_values)
