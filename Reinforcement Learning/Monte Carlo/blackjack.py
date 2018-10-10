44c84c93bececf3b6026f044a1d2d81a1822edf7
Hamifthi <hamedonil@yahoo.com>
2018-10-10 16:05:47 +0330

    to some extent it's ready

--- Reinforcement Learning/Monte Carlo/blackjack.py
+++ Reinforcement Learning/Monte Carlo/blackjack.py
@@ -6,14 +6,19 @@ env = gym.make('Blackjack-v0')
 
 def monte_carlo_prediction(policy, env, num_episodes, discount_factor = 1.0):
     policy = policy
-    V = np.zeros((22, 11, 2))
-    return_s = np.zeros((22, 11, 2))
+    V = []
+    return_sum = []
+    return_count = []
+    states_list  = []
+
     for episode in range(num_episodes):
         states = []
         G = []
+
         state = env.reset()
-        while state[0] > 21:
+        while state[0] > 21 and state[0] < 12:
             state = env.reset()
+
         while True:
             states.append(state)
             if state[0] == policy:
@@ -23,10 +28,20 @@ def monte_carlo_prediction(policy, env, num_episodes, discount_factor = 1.0):
             G.append(reward)
             if complete == True:
                 break
-            states.append(next_state)
-            
-        print(states)
-        print(G)
-        
+            state = next_state
 
-monte_carlo_prediction(20, env, 10, discount_factor = 1.0)
+        for state in range(len(states)):
+            if states[state] in states_list:
+                index = states_list.index(states_list[state])
+                return_count[index] += 1
+            else:
+                states_list.append(states[state])
+                return_count.append(1)
+                return_sum.append(sum([x * (discount_factor ** i) for i, x in enumerate(G[state:])]))
+    
+    for state in range(len(states_list)):
+        V.append([states_list[state], round(return_sum[state] / return_count[state], 2)])
+    
+    return np.array(V)
+
+print(monte_carlo_prediction(20, env, 10000, discount_factor = 1.0))
