0b5614fdf6a84be5781e8ce6c6e1a9bbf4df256e
Hamifthi <hamedonil@yahoo.com>
2018-10-15 00:57:43 +0330

    writing main function for monte carlo one visit

--- Reinforcement Learning/Monte Carlo/MC_on_policy_first_visit.py
+++ Reinforcement Learning/Monte Carlo/MC_on_policy_first_visit.py
@@ -6,14 +6,39 @@ from collections import defaultdict
 env = gym.make('Blackjack-v0')
 
 def make_epsilon_greedy_policy(Q, epsilon, nA):
-    policy = defaultdict(float)
+    policy = np.zeros(2)
     def policy_function(observation):
-        index = np.argmax(Q[observation])
-        policy[observation] = [epsilon/env.action_space.n, epsilon/env.action_space.n]
-        policy[observation][index] += 1 - epsilon
+        policy = [epsilon/env.action_space.n, epsilon/env.action_space.n]
+        policy[np.argmax(Q[observation])] += 1 - epsilon
         return policy
 
     return policy_function
 
-Q = defaultdict(lambda: np.zeros(env.action_space.n))
-print(make_epsilon_greedy_policy(Q, 0.5, env.action_space.n)(env.observation_space.sample()))
+def mc_control_epsilon_greedy(env, num_episodes, gamma = 1.0, epsilon = 0.1):
+    returns_sum = defaultdict(float)
+    returns_count = defaultdict(float)
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+
+    for episode in range(num_episodes):
+        state = env.reset()
+
+        while True:
+            states = []
+            rewards = []
+            G = 0
+
+            states.append(state)
+            action = np.argmax(policy(state))
+            next_state, reward, complete, _ = env.step(action)
+            rewards.append(reward)
+
+            if complete == True:
+                break
+            state = next_state
+
+        for step in states:
+            
+
+print(mc_control_epsilon_greedy(env, 10, gamma = 1.0, epsilon = 0.1))
