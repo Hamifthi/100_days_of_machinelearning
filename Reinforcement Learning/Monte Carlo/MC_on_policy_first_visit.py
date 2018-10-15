ca7d23261eefad7c482fc489428962c326c0a6cd
Hamifthi <hamedonil@yahoo.com>
2018-10-15 10:01:01 +0330

    it's almost ready

--- Reinforcement Learning/Monte Carlo/MC_on_policy_first_visit.py
+++ Reinforcement Learning/Monte Carlo/MC_on_policy_first_visit.py
@@ -18,11 +18,12 @@ def mc_control_epsilon_greedy(env, num_episodes, gamma = 1.0, epsilon = 0.1):
     returns_sum = defaultdict(float)
     returns_count = defaultdict(float)
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
-
     policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
 
     for episode in range(num_episodes):
         state = env.reset()
+        while state[0] > 21 and state[0] < 12:
+            state = env.reset()
 
         while True:
             states = []
@@ -33,12 +34,18 @@ def mc_control_epsilon_greedy(env, num_episodes, gamma = 1.0, epsilon = 0.1):
             action = np.argmax(policy(state))
             next_state, reward, complete, _ = env.step(action)
             rewards.append(reward)
-
+        
             if complete == True:
                 break
             state = next_state
-
-        for step in states:
             
+        for step in range(len(states) - 1, -1, -1):
+            G = gamma * G + rewards[step]
+            returns_count[states[step]] += 1
+            returns_sum[states[step]] = G
+            Q[states[step]] = returns_sum[states[step]] / returns_count[states[step]]
+            policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
 
-print(mc_control_epsilon_greedy(env, 10, gamma = 1.0, epsilon = 0.1))
+    return Q
+
+print(mc_control_epsilon_greedy(env, 500000, gamma = 1.0, epsilon = 0.1))
